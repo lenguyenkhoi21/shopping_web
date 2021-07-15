8136285e1b1afc83b299014d67a04b461e1f05e3
lenguyenkhoi21 <lenguyenkhoi21@gmail.com>
2021-07-15 16:54:03 +0700

    Finish Styles Authentication

--- Components/Authentication/Component.Authentication.js
+++ Components/Authentication/Component.Authentication.js
@@ -8,11 +8,15 @@ import { useRouter } from "next/router"
 export const Authentication = () => {
     const router = useRouter()
     const context = useContext(GlobalContext)
-
-    if (context.store.user.store !== null) {
-
-    }
-
+    const [isSignup, setIsSignup] = useState(false)
+    const [error, setError] = useState(false)
+    const [registerAccount, setRegisterAccount] = useState({
+        phone : '',
+        name : '',
+        email : '',
+        password : '',
+        verifiedPassword : ''
+    })
     const [account, setAccount] = useState({
         phone : '',
         password : ''
@@ -26,11 +30,59 @@ export const Authentication = () => {
         })
     }
 
+    const onChangedSignup = (evt) => {
+        const value = evt.target.value
+        setRegisterAccount({
+            ...registerAccount,
+            [evt.target.name]: value
+        })
+    }
+
+    const handleSignup = (event) => {
+        event.preventDefault()
+
+        if (registerAccount.password === registerAccount.verifiedPassword) {
+            const acc = {
+                phone: registerAccount.phone,
+                email: registerAccount.email,
+                password : registerAccount.password,
+                name: registerAccount.name
+            }
+            axios.post(`${API_DIR}/api/signup`, acc)
+                .then(value => {
+                    if (value.data.message === 'Success') {
+                        setError(false)
+                        const login = {
+                            phone : registerAccount.phone,
+                            password: registerAccount.password
+                        }
+
+                        axios.post(`${API_DIR}/api/login`, login)
+                            .then(res => {
+                                if (res.data.message === 'Success') {
+                                    router.push('/')
+                                    context.loginExport(login)
+                                }
+                            })
+                            .catch(reason => {
+                                setError(true)
+                            })
+                    }
+                })
+                .catch(reason => {
+                    setError(true)
+                })
+        }
+
+    }
+
     const handleSubmit = (event) => {
         event.preventDefault()
+
         axios.post(`${API_DIR}/api/login`, account)
             .then(value => {
                 if (value.data.message === 'Success') {
+                    setError(false)
                     const user = {
                         phone : value.data.phone,
                         token : value.data.token
@@ -42,55 +94,202 @@ export const Authentication = () => {
                 }
             })
             .catch(reason => {
-
+                console.log(reason)
+                setError(true)
             })
     }
-    return (
-        <div className={`container empty`}>
-            <div className={`row ${styles.around}`}>
-                <div className={`col-sm-4`} />
-                <div className={`col-sm-4 g-4 align-items-center`}>
-                    <div>
-                        <div className={`${styles.inputTxt}`}>
-                            <div className={`col-auto`}>
-                                <label className={`col-form-label`} > Số điện thoại </label>
-                            </div>
-                            <div className={`col-auto  ${styles.inputSize}`}>
-                                <input type='text' className={`form-control`} onChange={onChanged} name='phone' />
-                            </div>
-                            <br/>
-                            <div className={`col-auto`}>
-                                <label className={`col-form-label`} > Mật Khẩu </label>
-                            </div>
-                            <div className={`col-auto ${styles.inputSize}`}>
-                                <input type='password' className={`form-control`} onChange={onChanged} name='password' />
+
+    const changeScreen = (event) => {
+        event.preventDefault()
+        if (isSignup) {
+            setRegisterAccount({
+                phone : '',
+                name : '',
+                email : '',
+                password : '',
+                verifiedPassword : ''
+            })
+            setError(false)
+        } else {
+            setAccount({
+                phone : '',
+                password : ''
+            })
+            setError(false)
+        }
+        setIsSignup(!isSignup)
+    }
+
+    if (isSignup) {
+
+        return (
+            <div className={`container empty`}>
+                <div className={`container empty`}>
+                    <div className={`row ${styles.around}`}>
+                        <div className={`col-sm-4`} />
+                        <div className={`col-sm-4 g-4 align-items-center`}>
+                            <div>
+                                <h1 className={`${styles.txtCenter}`}> Đăng ký </h1>
+                                <div className={`${styles.inputTxt}`}>
+                                    <div className={`col-auto`}>
+                                        <label className={`col-form-label`} > Số điện thoại </label>
+                                    </div>
+                                    <div className={`col-auto  ${styles.inputSize}`}>
+                                        <input
+                                            type='text'
+                                            className={`form-control`}
+                                            onChange={onChangedSignup}
+                                            name='phone'
+                                        />
+                                    </div>
+                                    <br/>
+
+                                    <div className={`col-auto`}>
+                                        <label className={`col-form-label`} > Email </label>
+                                    </div>
+                                    <div className={`col-auto  ${styles.inputSize}`}>
+                                        <input
+                                            type='email'
+                                            className={`form-control`}
+                                            onChange={onChangedSignup}
+                                            name='email'
+                                        />
+                                    </div>
+                                    <br/>
+
+                                    <div className={`col-auto`}>
+                                        <label className={`col-form-label`} > Tên </label>
+                                    </div>
+                                    <div className={`col-auto  ${styles.inputSize}`}>
+                                        <input
+                                            type='text'
+                                            className={`form-control`}
+                                            onChange={onChangedSignup}
+                                            name='name'
+                                        />
+                                    </div>
+                                    <br/>
+
+                                    <div className={`col-auto`}>
+                                        <label className={`col-form-label`} > Mật Khẩu </label>
+                                    </div>
+                                    <div className={`col-auto ${styles.inputSize}`}>
+                                        <input
+                                            type='password'
+                                            className={`form-control`}
+                                            onChange={onChangedSignup}
+                                            name='password' />
+                                    </div>
+                                    <br/>
+
+                                    <div className={`col-auto`}>
+                                        <label className={`col-form-label`} > Nhập lại mật khẩu </label>
+                                    </div>
+                                    <div className={`col-auto ${styles.inputSize}`}>
+                                        <input
+                                            type='password'
+                                            className={`form-control`}
+                                            onChange={onChangedSignup}
+                                            name='verifiedPassword'
+                                        />
+                                    </div>
+                                </div>
+
+                                <div className={`col-auto ${styles.btnContainer}`}>
+                                    <button
+                                        type='submit'
+                                        className={`btn btn-primary mb-3 ${styles.btnLogin}`}
+                                        onClick={handleSignup}>
+                                        Đăng Ký
+                                    </button>
+
+                                    <button
+                                        type='submit'
+                                        className={`btn btn-primary mb-3 ${styles.btnLogout}`}
+                                        onClick={changeScreen}>
+                                        Đăng Nhập
+                                    </button>
+                                </div>
+
+                                <div className={styles.clearFix} />
+
+                                <div className={`col-auto ${styles.errorText}`}>
+                                    { error ? <p> Có lỗi xảy ra, vui lòng thử lại !</p> : null}
+                                </div>
+
                             </div>
                         </div>
-
-                        <div className={`col-auto ${styles.btnContainer}`}>
-                            <button
-                                type='submit'
-                                className={`btn btn-primary mb-3 ${styles.btnLogin}`}
-                                onClick={handleSubmit}>
-                                Đăng Nhập
-                            </button>
-
-                            <button
-                                type='submit'
-                                className={`btn btn-primary mb-3 ${styles.btnLogout}`}
-                                onClick={handleSubmit}>
-                                Đăng Ký
-                            </button>
-                        </div>
-
+                        <div className={`col-sm-4`} />
                     </div>
                 </div>
-                <div className={`col-sm-4`} />
             </div>
+        )
+    } else {
+
+        return (
+            <div className={`container empty`}>
+                <div className={`row ${styles.around}`}>
+                    <div className={`col-sm-4`} />
+                    <div className={`col-sm-4 g-4 align-items-center`}>
+                        <div>
+                            <h1 className={`${styles.txtCenter}`}> Đăng nhập </h1>
+                            <div className={`${styles.inputTxt}`}>
+                                <div className={`col-auto`}>
+                                    <label className={`col-form-label`} > Số điện thoại </label>
+                                </div>
+                                <div className={`col-auto  ${styles.inputSize}`}>
+                                    <input
+                                        type='text'
+                                        className={`form-control`}
+                                        onChange={onChanged}
+                                        name='phone'
+                                    />
+                                </div>
+                                <br/>
+                                <div className={`col-auto`}>
+                                    <label className={`col-form-label`} > Mật Khẩu </label>
+                                </div>
+                                <div className={`col-auto ${styles.inputSize}`}>
+                                    <input
+                                        type='password'
+                                        className={`form-control`}
+                                        onChange={onChanged}
+                                        name='password'
+                                    />
+                                </div>
+                            </div>
+
+                            <div className={`col-auto ${styles.btnContainer}`}>
+                                <button
+                                    type='submit'
+                                    className={`btn btn-primary mb-3 ${styles.btnLogin}`}
+                                    onClick={handleSubmit}>
+                                    Đăng Nhập
+                                </button>
+
+                                <button
+                                    type='submit'
+                                    className={`btn btn-primary mb-3 ${styles.btnLogout}`}
+                                    onClick={changeScreen}>
+                                    Đăng Ký
+                                </button>
+                            </div>
+
+                            <div className={styles.clearFix} />
+
+                            <div className={`col-auto ${styles.errorText}`}>
+                                { error ? <p> Có lỗi xảy ra, vui lòng thử lại !</p> : null}
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className={`col-sm-4`} />
+                </div>
+            </div>
+        )
+    }
 
 
-        </div>
-    )
 }
 
 
